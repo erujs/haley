02bfa2277fc19a13f66ce9dc0207cf58f5708224
Jerus M. Aquino <ljrsqn@gmail.com>
2018-03-10 06:04:35 +0800

    minor fix

--- src/Main/Interface.java
+++ src/Main/Interface.java
@@ -5,64 +5,79 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/**
+ * Created by Eru on 4/1/2016.
+ */
 public class Interface {
 
-    public JFrame frmHalley;
+    public JFrame frmHaley;
     public JTextField tf_response;
-    private JLabel lbl_screen;
-    private TextArea ta_convo;
-    private Algo r;
+    public JLabel lbl_screen;
+    public TextArea ta_convo;
+    private static Algo r;
 
-    public void initialize() {
-        frmHalley = new JFrame();
-        frmHalley.setTitle("Haley Lifehack Support");
-        frmHalley.setBounds(100, 100, 701, 700);
-        frmHalley.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frmHalley.getContentPane().setLayout(null);
-        frmHalley.setLocationRelativeTo(null);
+    public void init() {
+        frmHaley = new JFrame();
+        frmHaley.setBounds(100, 100, 701, 700);
+        frmHaley.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmHaley.getContentPane().setLayout(null);
+        frmHaley.setLocationRelativeTo(null);
 
         tf_response = new JTextField();
         tf_response.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent arg0) {
                 if(arg0.getKeyCode()==KeyEvent.VK_ENTER){
-
                     String response = tf_response.getText();
-
                     tf_response.setText("");
                     ta_convo.append("You: "+response+"\n");
-                    Algo reply = new Algo();
-                    r=new Algo();
+                    r = new Algo();
+                    r.reply(response);
                 }
             }
 
         });
         tf_response.setBounds(31, 513, 375, 31);
-        frmHalley.getContentPane().add(tf_response);
+        frmHaley.getContentPane().add(tf_response);
         tf_response.setColumns(10);
 
         ta_convo = new TextArea();
         ta_convo.setEditable(false);
         ta_convo.setBounds(31, 376, 375, 126);
-        frmHalley.getContentPane().add(ta_convo);
+        frmHaley.getContentPane().add(ta_convo);
 
         lbl_screen = new JLabel("");
         lbl_screen.setBounds(31, 24, 339, 339);
-        frmHalley.getContentPane().add(lbl_screen);
+        frmHaley.getContentPane().add(lbl_screen);
 
         JLabel lbl_background = new JLabel("");
-        lbl_background.setIcon(new ImageIcon(Main.class.getResource("/pics/Halley.jpg")));
+        lbl_background.setIcon(new ImageIcon(Main.class.getResource("/pics/Haley" + ".jpg")));
         lbl_background.setBounds(-17, 11, 775, 661);
-        frmHalley.getContentPane().add(lbl_background);
+        frmHaley.getContentPane().add(lbl_background);
 
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    frmHalley.setVisible(true);
+                    frmHaley.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
     }
+//    public void reply(String statement) {
+//        String haley = statement;
+//        haley = haley.toLowerCase();
+//
+//        if(haley.contains("tshirt")|| haley.contains("shirt")) {
+//            ta_convo.append("Haley: A lifehack for you ;)\n");
+//            ImageIcon img1 = new ImageIcon(Main.class.getResource("/pics/examplePic.jpg"));
+//            lbl_screen.setIcon(img1);
+//        }
+//        else{
+//            ta_convo.append("Haley: I dont know what your talking about\n");
+//            ImageIcon img1 = new ImageIcon(Main.class.getResource("/pics/blank.png"));
+//            lbl_screen.setIcon(img1);
+//        }
+//    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,15 +1,13 @@
 package Main;
 
-import java.security.AlgorithmParameterGenerator;
-
 /**
- * Created by Eru on 3/19/2016.
+ * Created by Eru on 4/1/2016.
  */
 public class Main {
     private static Interface ui;
 
     public static void main(String[] args) {
         ui = new Interface();
-        ui.initialize();
+        ui.init();
     }
 }
